d9cf0bf727259b3aa4b66bd21eae3c05f981eb7a
손성수 <sungsu7437@naver.com>
2019-03-10 00:06:17 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const os = require('os');
+
+var handler = function(request, response) {
+    response.writeHead(200);
+    response.end("Hello World\n");
+};
+
+var www = http.createServer(handler);
+www.listen(8080);
